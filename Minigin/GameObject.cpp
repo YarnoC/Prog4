625d1c1e5f246bdb673212766ef63dc4dbb31448
Yarno <yarno.ceulemans@student.howest.be>
2025-02-13 17:08:39 +0100

    feat: now update all components
    also the text rendering now works as a result of that

--- Minigin/GameObject.cpp
+++ Minigin/GameObject.cpp
@@ -2,14 +2,31 @@
 #include "GameObject.h"
 #include "ResourceManager.h"
 #include "Renderer.h"
+#include "TextComponent.h"
 
 dae::GameObject::~GameObject() = default;
 
-void dae::GameObject::Update(){}
+void dae::GameObject::Update()
+{
+	for (auto&& comp : m_ComponentVec)
+	{
+		comp->Update();
+	}
+}
 
 void dae::GameObject::Render() const
 {
+	//TODO: make this not shit and work with a render component of which everything that needs to be rendered can inherit cause this sucks
 	//const auto& pos = m_transform.GetPosition();
+	for (auto&& comp : m_ComponentVec) //universal ref
+	{
+		auto renderPtr = dynamic_cast<TextComponent*>(comp.get());
+
+		if (renderPtr)
+		{
+			renderPtr->Render();
+		}
+	}
 	//Renderer::GetInstance().RenderTexture(*m_texture, pos.x, pos.y);
 }
 
