4fe63c1b0eb347c8508bd825b3f75650f621ae4b
Yarno <yarno.ceulemans@student.howest.be>
2025-03-10 00:06:57 +0100

    feat: implemented gamepad input checks

--- Minigin/GamepadHandler.cpp
+++ Minigin/GamepadHandler.cpp
@@ -7,5 +7,39 @@ GamepadHandler::GamepadHandler()
 
 Command* GamepadHandler::HandleGamepadInput()
 {
+	//polling
+	CopyMemory(&m_PreviousState, &m_CurrentState, sizeof(XINPUT_STATE));
+	ZeroMemory(&m_CurrentState, sizeof(XINPUT_STATE));
+	DWORD dwResult = XInputGetState(m_GamepadIndex, &m_CurrentState);
+
+	if (dwResult != ERROR_SUCCESS) return;
+
+	auto buttonChanges = m_CurrentState.Gamepad.wButtons ^ m_PreviousState.Gamepad.wButtons;
+	m_ButtonsPressedThisFrame = buttonChanges ^ m_CurrentState.Gamepad.wButtons;
+	m_ButtonsReleasedThisFrame = buttonChanges & (~m_CurrentState.Gamepad.wButtons);
+
+	if (IsDownThisFrame(XINPUT_GAMEPAD_DPAD_LEFT)) return m_LeftCmd;
+	if (IsDownThisFrame(XINPUT_GAMEPAD_DPAD_RIGHT)) return m_RightCmd;
+	if (IsDownThisFrame(XINPUT_GAMEPAD_DPAD_UP)) return m_UpCmd;
+	if (IsDownThisFrame(XINPUT_GAMEPAD_DPAD_DOWN)) return m_DownCmd;
+
 	return nullptr;
+}
+
+//got pressed this frame
+bool GamepadHandler::IsDownThisFrame(unsigned int button) const
+{
+	return m_ButtonsPressedThisFrame & button;
+}
+
+//released this frame
+bool GamepadHandler::IsUpThisFrame(unsigned int button) const
+{
+	return m_ButtonsReleasedThisFrame & button;
+}
+
+//is down this frame, either already or pressed this frame
+bool GamepadHandler::IsPressed(unsigned int button) const
+{
+	return m_CurrentState.Gamepad.wButtons ^ button;
 }
--- Minigin/GamepadHandler.h
+++ Minigin/GamepadHandler.h
@@ -1,5 +1,9 @@
 #pragma once
-#include "Xinput.h"
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#include "XInput.h"
+#pragma comment(lib, "xinput.lib")
+
 #include "Command.h"
 #include <memory>
 
@@ -11,5 +15,20 @@ public:
 
 private:
 	std::unique_ptr<Command> m_MoveCommand;
+	uint8_t m_GamepadIndex{ 0 }; //placeholder for now (?)
+
+	XINPUT_STATE m_CurrentState{};
+	XINPUT_STATE m_PreviousState{};
+	DWORD m_ButtonsPressedThisFrame{};
+	DWORD m_ButtonsReleasedThisFrame{};
+
+	Command* m_LeftCmd{ nullptr };
+	Command* m_RightCmd{ nullptr };
+	Command* m_UpCmd{ nullptr };
+	Command* m_DownCmd{ nullptr };
+
+	bool IsDownThisFrame(unsigned int button) const;
+	bool IsUpThisFrame(unsigned int button) const;
+	bool IsPressed(unsigned int button) const;
 };
 
