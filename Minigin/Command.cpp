740387b2060223097417e28292bf37efd77d9f08
Yarno <yarno.ceulemans@student.howest.be>
2025-03-10 17:29:07 +0100

    perf: got rid of need for moveComp for input

--- Minigin/Command.cpp
+++ Minigin/Command.cpp
@@ -2,6 +2,7 @@
 #include "MoveComponent.h"
 #include "GameObject.h"
 #include <cassert>
+#include "GameTime.h"
 
 GameActorCommand::GameActorCommand(dae::GameObject* actor)
     : m_Actor{actor}
@@ -23,5 +24,10 @@ MoveActorCommand::MoveActorCommand(dae::GameObject* actor, glm::vec2 moveVec)
 
 void MoveActorCommand::Execute()
 {
-    m_MoveComponent->Move(m_MoveVec);
+    auto actor = GetGameActor();
+    auto loc = actor->GetLocalPosition();
+    loc += m_MoveVec;
+    auto dt = GameTime::GetDt();
+    actor->SetLocalPosition({ loc.x * dt, loc.y * dt, 0 });
+    //m_MoveComponent->Move(m_MoveVec);
 }
