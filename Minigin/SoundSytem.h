32d536c268a7c94369a0230331b8ba197528e5ea
Yarno <yarno.ceulemans@student.howest.be>
2025-04-17 22:21:56 +0200

    feat: added null sound system
    fix: fixed bugs in null sound system

--- Minigin/SoundSytem.h
+++ Minigin/SoundSytem.h
@@ -5,13 +5,26 @@ namespace dae
 class SoundSytem
 {
 public:
-	virtual ~SoundSystem() = default;
 	virtual void Play(const unsigned short soundId, const float volume) = 0;
+	virtual ~SoundSytem() = default;
 
-	SoundSystem(const SoundSytem&) = delete;
-	SoundSystem(SoundSystem&&) = delete;
-	SoundSystem& operator=(const SoundSystem&) = delete;
-	SoundSystem& operator(SoundSystem&&) = delete;
+	SoundSytem(const SoundSytem&) = delete;
+	SoundSytem(SoundSytem&&) = delete;
+	SoundSytem& operator=(const SoundSytem&) = delete;
+	SoundSytem& operator(SoundSytem&&) = delete;
+};
+
+class NullSoundSystem final : public SoundSytem
+{
+public:
+	void Play(const unsigned short soundId, const float volume) {};
+
+	~NullSoundSystem() = default;
+
+	NullSoundSystem(const NullSoundSystem&) = delete;
+	NullSoundSystem(NullSoundSystem&&) = delete;
+	NullSoundSystem& operator=(const NullSoundSystem&) = delete;
+	NullSoundSystem& operator=(NullSoundSystem&&) = delete;
 };
 
 }
