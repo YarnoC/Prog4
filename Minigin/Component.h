bce6ba46ead1b39649f28ab507a333c2a3b1d182
Yarno <yarno.ceulemans@student.howest.be>
2025-06-03 17:11:38 +0200

    fix: fixed typo

--- Minigin/Component.h
+++ Minigin/Component.h
@@ -15,20 +15,20 @@ public:
     virtual inline void FixedUpdate() {};
     virtual inline void Render() const {};
 
-    //marks as terminal and will be destroyed at and of late update
+    //marks as terminal and will be destroyed at end of late update
     void Destroy();
     bool IsTerminal();
 
     dae::GameObject* GetOwner() const;
+
+private:
+    dae::GameObject* m_OwnerPtr;
 
 protected:
     bool m_IsTerminal{ false };
     
 
     Component(dae::GameObject* ownerPtr);
-
-private:
-    dae::GameObject* m_OwnerPtr;
 };
 
 }
