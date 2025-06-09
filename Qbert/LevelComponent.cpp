6b309cb75172a906a131f81d420521fda6da3233
Yarno <yarno.ceulemans@student.howest.be>
2025-06-09 15:20:36 +0200

    feat: set up level comp ctor

--- Qbert/LevelComponent.cpp
+++ Qbert/LevelComponent.cpp
@@ -16,10 +16,12 @@ void LevelComponent::InitLevel(int level)
 	//move the discs around based on the file stuff
 }
 
-LevelComponent::LevelComponent(dae::GameObject* owner, dae::Scene& /*scene*/, int level) :
+LevelComponent::LevelComponent(dae::GameObject* owner, dae::Scene& scene, int level) :
 	dae::Component(owner)
 {
 	m_Level.reserve(7);
+	m_CubeSpriteSheet = std::make_unique<dae::Texture2D>("CubeSprite.png");
+
 	for (int col{}; col < 7; ++col)
 	{
 		m_Level[col].reserve(col + 1);
@@ -27,6 +29,10 @@ LevelComponent::LevelComponent(dae::GameObject* owner, dae::Scene& /*scene*/, in
 		for (int i{}; i < col + 1; ++i)
 		{
 			auto cubeObj = std::make_unique<dae::GameObject>();
+			cubeObj->SetParent(GetOwner(), false);
+			auto cubeComp = cubeObj->AddComponent<CubeComponent>(m_CubeSpriteSheet.get(), level);
+			m_Level[col][i] = cubeComp;
+			scene.Add(cubeObj);
 		}
 	}
 
